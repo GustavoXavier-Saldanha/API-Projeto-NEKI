3a3799dd2e6437b9797a9867169656270f78f6b7
Gustavo Xavier Saldanha <82043764+GustavoXavier-Saldanha@users.noreply.github.com>
2021-08-30 08:26:19 -0300

    Add files via upload
    Início da aplicação

---
+++ BackNeki/src/main/java/com/example/demo/BackNekiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackNekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackNekiApplication.class, args);
+	}
+
+}
